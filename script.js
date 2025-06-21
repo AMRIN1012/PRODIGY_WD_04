e3adc017ff9b6731192e18e096ca97c6d6ba839e
AMRIN1012 <10amrinami12@gmail.com>
2025-06-21 17:45:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Navbar shadow effect
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
